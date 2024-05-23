3ee970f47fa2707e9dfda773e760fa2e97c805f2
Tushar Patil <49657859+CyberArmorKnight@users.noreply.github.com>
2024-05-23 13:35:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -48,3 +48,62 @@ scrollToTopBtn.addEventListener('click', () => {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 });
 
+
+document.addEventListener('DOMContentLoaded', () => {
+    const mainTextElement = document.getElementById('mainText');
+    const subTextElement = document.getElementById('subText');
+    const mainTexts = ["Tushar Santosh Patil", "CyberArmorKnight"];
+    const subTexts = ["Cyber Security Analyst", "Security Consultant", "Cyber Security Instructor"];
+    let mainTextIndex = 0;
+    let subTextIndex = 0;
+    let mainCharIndex = 0;
+    let subCharIndex = 0;
+  
+    function typeMainText() {
+      if (mainCharIndex < mainTexts[mainTextIndex].length) {
+        mainTextElement.innerHTML += mainTexts[mainTextIndex].charAt(mainCharIndex);
+        mainCharIndex++;
+        setTimeout(typeMainText, 100); // Adjust typing speed (ms)
+      } else {
+        setTimeout(eraseMainText, 1000); // Wait before erasing (ms)
+      }
+    }
+  
+    function eraseMainText() {
+      if (mainCharIndex > 0) {
+        mainTextElement.innerHTML = mainTexts[mainTextIndex].substring(0, mainCharIndex - 1);
+        mainCharIndex--;
+        setTimeout(eraseMainText, 50); // Adjust erasing speed (ms)
+      } else {
+        mainTextIndex = (mainTextIndex + 1) % mainTexts.length;
+        setTimeout(typeMainText, 500); // Wait before typing next text (ms)
+      }
+    }
+  
+    function typeSubText() {
+      if (subCharIndex < subTexts[subTextIndex].length) {
+        subTextElement.innerHTML += subTexts[subTextIndex].charAt(subCharIndex);
+        subCharIndex++;
+        setTimeout(typeSubText, 100); // Adjust typing speed (ms)
+      } else {
+        setTimeout(eraseSubText, 1000); // Wait before erasing (ms)
+      }
+    }
+  
+    function eraseSubText() {
+      if (subCharIndex > 0) {
+        subTextElement.innerHTML = subTexts[subTextIndex].substring(0, subCharIndex - 1);
+        subCharIndex--;
+        setTimeout(eraseSubText, 50); // Adjust erasing speed (ms)
+      } else {
+        subTextIndex = (subTextIndex + 1) % subTexts.length;
+        setTimeout(typeSubText, 500); // Wait before typing next text (ms)
+      }
+    }
+  
+    // Initial calls to start the typing effects
+    typeMainText();
+    typeSubText();
+  });
+
+
