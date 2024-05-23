faf614789a86aff610c8bbe241abaf53728fcf19
Tushar Patil <49657859+CyberArmorKnight@users.noreply.github.com>
2024-05-23 22:19:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -180,3 +180,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-J9LTTL9PFT"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-J9LTTL9PFT');
+</script>
+
+
+
