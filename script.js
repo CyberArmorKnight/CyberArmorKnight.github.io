c1c786b88af28acfbe30b8c2c9482075a6abf1e5
Tushar Patil <49657859+CyberArmorKnight@users.noreply.github.com>
2024-06-01 00:19:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -68,3 +68,21 @@ function updateSubText() {
 // Initial display
 updateSubText();
 setInterval(updateSubText, 3000); 
+
+
+
+// Scroll to Top Button
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
+
+scrollToTopBtn.addEventListener('click', () => {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
